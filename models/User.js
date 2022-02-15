3f7ff3e9a6bc443f005d2c6d6d8beb0560448808
Najla Parker <najla08@gmail.com>
2022-02-14 17:57:54 -0700

    Updated config, userController, User.js & server.js

--- models/User.js
+++ models/User.js
@@ -1,50 +1,38 @@
 const { Model, DataTypes, UUIDV4 } = require('sequelize');
 const sequelize = require('../config');
-
-class User extends Model {};
-
+class User extends Model {}
 User.init(
-    {
-        id: {
-            type: DataTypes.UUID,
-            defaultValue: UUIDV4,
-            primaryKey: true,
-        },
-        username: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-            validate: {
-                isEmail: true,
-            }
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [6]
-            }
-        },
-
-        status: {
-            type: DataTypes.STRING,
-            enum: ['Pending', 'Active'],
-            default: 'Pending'
-        },
-        confirmationCode: {
-            type: DataTypes.STRING,
-            unique: true },
-    },
-    {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        modelName: 'user',
-    }
+	{
+		id: {
+			type: DataTypes.UUID,
+			defaultValue: UUIDV4,
+			primaryKey: true,
+		},
+		username: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		email: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true,
+			validate: {
+				isEmail: true,
+			}
+		},
+		password: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				len: [6]
+			}
+		}
+	},
+	{
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		modelName: 'user',
+	}
 );
-
 module.exports = User;
